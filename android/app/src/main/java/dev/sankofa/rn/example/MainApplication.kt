5587069e688a849bb64af29bb2ffac0fc836a4d4
saytoonz <35128520+saytoonz@users.noreply.github.com>
2026-04-18 15:51:54 +0000

    feat: update version to 1.0.9, enable cleartext traffic in AndroidManifest, and add expo-build-properties dependency

--- android/app/src/main/java/dev/sankofa/rn/example/MainApplication.kt
+++ android/app/src/main/java/dev/sankofa/rn/example/MainApplication.kt
@@ -12,10 +12,10 @@ import com.facebook.react.ReactHost
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
-import dev.sankofa.rn.SankofaDeployBundleProvider
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
+import dev.sankofa.rn.SankofaDeployBundleProvider
 
 class MainApplication : Application(), ReactApplication {
 
@@ -30,12 +30,10 @@ class MainApplication : Application(), ReactApplication {
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
 
-          override fun getJSBundleFile(): String? {
-            return SankofaDeployBundleProvider.getJSBundleFile(applicationContext)
-              ?: super.getJSBundleFile()
-          }
-
-          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+              override fun getJSBundleFile(): String? {
+      return SankofaDeployBundleProvider.getJSBundleFile(applicationContext) ?: super.getJSBundleFile()
+    }
+override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
           override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
       }
